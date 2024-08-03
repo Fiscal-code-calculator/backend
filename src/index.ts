67b845680d59daf110dda119f8e19feb6c9d5628
antoniotassone94 <antonio.tassone@studio.unibo.it>
2024-08-03 06:22:53 +0200

    first commit backend application.

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port:number = 3000;
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.get("/",(req,res) => res.send("Hello world!"));
+
+server.listen(port,() => {
+    console.log("Server is listening the port 3000.");
+});
